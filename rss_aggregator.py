feefe44060b860ca05433f24e1a259225a53be4c
MickB74 <mickeybarry@gmail.com>
2025-08-28 23:22:34 -0400

    Add EIA Google Alert feed for energy data and statistics

--- rss_aggregator.py
+++ rss_aggregator.py
@@ -103,6 +103,11 @@ class RSSAggregator:
                 'url': 'https://www.google.com/alerts/feeds/11148815731617361241/10430378286967577760',
                 'name': 'Google Alert: LNG',
                 'keywords': ['energy', 'power', 'electricity', 'energy market', 'energy policy', 'energy transition']
+            },
+            {
+                'url': 'https://www.google.com/alerts/feeds/11148815731617361241/8761439016609917737',
+                'name': 'Google Alert: EIA',
+                'keywords': ['eia', 'energy information administration', 'energy data', 'energy statistics', 'energy reports', 'energy market data']
             }
         ]
         
