958e59504dcbb6dba86ae7646d698967655bdc7d
MickB74 <mickeybarry@gmail.com>
2025-09-21 19:26:46 -0400

    feat: Add script to automate news updates

---
+++ update_news.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Navigate to the script's directory
+cd "$(dirname "$0")"
+# Activate the virtual environment
+source venv/bin/activate
+# Run the python script
+python3 update_news.py
